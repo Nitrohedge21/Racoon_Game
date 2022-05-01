66bc1d2aff4f53609105e410089a0fc891ea6670
Nitrohedge21 <98889986+Nitrohedge21@users.noreply.github.com>
2022-05-01 21:38:31 +0100

    check readme

--- Unnamed Racoon Game/Assets/Scripts/GrabController.cs
+++ Unnamed Racoon Game/Assets/Scripts/GrabController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GrabController : MonoBehaviour
 {
+    private bool playerIsHolding;
     public Transform GrabDetect;
     public Transform GrabDetect2;
     public Transform BoxHolder;
@@ -11,13 +12,15 @@ public class GrabController : MonoBehaviour
     public float rayDist;
     private Transform Microwave;
     private Transform Microwave2;
-
+    [SerializeField] private AudioSource ThrowingSound;
     void Update()
     {
         RaycastHit2D grabCheck = Physics2D.Raycast(GrabDetect.position, Vector2.right * transform.localScale, rayDist);
         // The second part, "or" section, is for the new item that the npc spawns in
         if (grabCheck.collider != null && grabCheck.collider.tag == "Box" || grabCheck.collider != null &&  grabCheck.collider.tag == "TradeBox" || grabCheck.collider != null && grabCheck.collider.tag == "CombinedItem")
         {
+            //_ = playerIsHolding == true;
+            
             //So i added an input to unity project settings and tried to use that
             //Input.GetButtonDown("Grab") similar to movement inputs but it didn't work out, the player kept dropping the box
             if (Input.GetKey(KeyCode.G))
@@ -27,14 +30,15 @@ public class GrabController : MonoBehaviour
                 grabCheck.collider.gameObject.transform.position = BoxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
                 //Debug.Log("Item grabbed");
-                //Had to comment it out because it kept getting spammed for some reason
-                //Might be because there are multiple versions of it?
+                ThrowingSound.Play();
+                //This does work, but it can be played even when the player isn't holding anything
             }
             else
             {
                 // This part is to drop the box/object
                 grabCheck.collider.gameObject.transform.parent = null;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                
                 //Might change .isKinematic to gravityscale = 0f because apparently is causes some glitches.
                 //Debug.Log("Item dropped");
                 //Check the first grab function to see the reason why it's commented out
@@ -46,12 +50,15 @@ public class GrabController : MonoBehaviour
 
             if (grabCheck2.collider != null && grabCheck2.collider.tag == "Box" || grabCheck2.collider != null &&  grabCheck2.collider.tag == "TradeBox" || grabCheck2.collider != null && grabCheck2.collider.tag == "CombinedItem")
             {
+                //_ = playerIsHolding == true;
+
                 if (Input.GetKey(KeyCode.G))
-                {
+            {
                     // This part is to grab the box/object
                     grabCheck2.collider.gameObject.transform.parent = BoxHolder2;
                     grabCheck2.collider.gameObject.transform.position = BoxHolder2.position;
                     grabCheck2.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                ThrowingSound.Play();
                 //Debug.Log("Item grabbed");
                 //Check the first grab function to see the reason why it's commented out
             }
@@ -60,9 +67,9 @@ public class GrabController : MonoBehaviour
                     // This part is to drop the box/object
                     grabCheck2.collider.gameObject.transform.parent = null;
                     grabCheck2.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                    //Debug.Log("Item dropped");
-                    //Check the first grab function to see the reason why it's commented out
-                }
+                //Debug.Log("Item dropped");
+                //Check the first grab function to see the reason why it's commented out
+            }
             }
         }
     }
